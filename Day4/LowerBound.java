5cec02cf22b858332a70d868d3c98262f9ed6ca3
ChiragMalav <92642325+ChiragMalav@users.noreply.github.com>
2025-08-09 21:43:45 +0530

    Create LowerBound.java

---
+++ Day4/LowerBound.java
@@ -0,0 +1,21 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        // code here
+        int n= arr.length;
+        int ans = n;
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            
+            if(arr[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
