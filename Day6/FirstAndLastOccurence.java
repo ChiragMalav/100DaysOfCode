288d05c5c84d76051d01c25eb920f1b8aab7ef94
ChiragMalav <92642325+ChiragMalav@users.noreply.github.com>
2025-08-12 11:24:13 +0530

    Create FirstAndLastOccurence.java

---
+++ Day6/FirstAndLastOccurence.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int n = nums.length;
+
+        int low = 0;
+        int high = n-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans[0]=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        low=0;
+        high=n-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                ans[1]=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        
+        return ans;
+
+    }
+}
