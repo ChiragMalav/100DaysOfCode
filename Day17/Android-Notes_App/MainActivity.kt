3f7304cef9aa432662ea16411429e2ee9d056873
ChiragMalav <92642325+ChiragMalav@users.noreply.github.com>
2025-08-23 20:24:45 +0530

    Create MainActivity.kt

---
+++ Day17/Android-Notes_App/MainActivity.kt
@@ -0,0 +1,16 @@
+class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application,noteRepository)
+        noteViewModel = ViewModelProvider(this,viewModelProviderFactory)[NoteViewModel::class.java]
+    }
+}
