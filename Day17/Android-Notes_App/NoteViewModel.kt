799e9c2df7356e2014a34130ad5a1110e3cb223a
ChiragMalav <92642325+ChiragMalav@users.noreply.github.com>
2025-08-23 20:26:49 +0530

    Create NoteViewModel.kt

---
+++ Day17/Android-Notes_App/NoteViewModel.kt
@@ -0,0 +1,21 @@
+class NoteViewModel(app : Application, private val noteRepository: NoteRepository) : AndroidViewModel(app){
+
+    fun addNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.insertNote(note)
+        }
+
+    fun deleteNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.deleteNote(note)
+        }
+
+    fun updateNote(note: Note) =
+        viewModelScope.launch {
+            noteRepository.updateNote(note)
+        }
+
+    fun getAllNotes() = noteRepository.getAllNotes()
+
+    fun searchNote(query : String?) = noteRepository.searchNoteQuery(query)
+}
